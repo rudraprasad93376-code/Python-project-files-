55daae405aae73107929b0c8d4e094cfd5c6fb79
RUDRA prasad mallick <rudraprasad93376@gmail.com>
2025-09-15 18:35:29 +0530

    Currency Converter

---
+++ Currency Converter .py
@@ -0,0 +1,10 @@
+# 1st insatll (forex python) use vs code terminal (pip install forex-python)
+
+from forex_python.converter import CurrencyRates
+
+c = CurrencyRates()
+amount = 1
+from_currency='USD'
+to_currency = 'INR'
+converted_amount = c.convert(from_currency, to_currency, amount)
+print(amount, from_currency, "=", converted_amount, to_currency)
